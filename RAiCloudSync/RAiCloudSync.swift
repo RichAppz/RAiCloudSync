f6aa97d9a3d56a632642c833449c63c27e8304cf
Rich Abery <rich@zappidev.com>
2016-07-10 10:06:23 +0100

    Addition of multiple keys and extra functionality for this

--- RAiCloudSync/RAiCloudSync.swift
+++ RAiCloudSync/RAiCloudSync.swift
@@ -1,53 +1,98 @@
 //
-//  iCloudSync.swift
+//  RAiCloudSync.swift
 //  RichAppz
 //
-//  Created by Rich Abery on 10/07/2016.
 //  Copyright © 2016 RichAppz Limited. All rights reserved.
-//  richappz.com
+//  richappz.com - (rich@richappz.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 
 import UIKit
 
-public let raCloudSyncNotification: String = "raCloudSyncNotificaion"
+public let raCloudSyncNotification: String = "raiCloudSyncNotification"
 
-class iCloudSyncListenr {
+class RAiCloudSync {
     
     //================================================================================
     // MARK: Variables
     //================================================================================
     
-    var prefix: String = ""
+    var keys        = [String]()
+    var isSyncing   = false
+    
     
     //================================================================================
     // MARK: Singleton
     //================================================================================
     
-    static let manager = iCloudSyncListenr()
+    static let manager = RAiCloudSync()
     private init() {}
+    
     
     //================================================================================
     // MARK: Start Service
     //================================================================================
     
     /// Start the iCloudSync with the prefix that you would like to listen for
-    /// :params: prefix: String value of the key required
-    static func startWithPrefix(prefix: String) {
-        manager.prefix = prefix
+    /// :params: keys: [String] value of the keys required
+    internal static func start(withKeys keys: [String]) {
+        manager.keys        = keys
+        manager.isSyncing   = true
         
         // Set up notifiers for iCloudSync
         setListenerFromCloud()
         setListenerForNotificationChange()
-        NSNotificationCenter.defaultCenter().postNotificationName(raCloudSyncNotification, object: nil)
         
         let cloudStorage = NSUbiquitousKeyValueStore.defaultStore()
         let dict = cloudStorage.dictionaryRepresentation
         for (key, value) in dict {
-            if key.hasPrefix(manager.prefix) {
+            if manager.keys.contains(key) {
                 NSUserDefaults.standardUserDefaults().setObject(value, forKey: key)
             }
         }
     }
+    
+    /// Remove a key stored in the manager
+    /// :params: key: String value of the key to be removed
+    internal static func remove(key key: String) {
+        manager.keys.removeObject(key)
+        if manager.keys.count == 0 { removeAllObservers() }
+    }
+    
+    /// Listen for a new key
+    /// :params: key: String value of the key to be added
+    internal static func add(key key: String) {
+        manager.keys.append(key)
+        if !manager.isSyncing {
+            setListenerFromCloud()
+            setListenerForNotificationChange()
+        }
+    }
+    
+    /// Stops and resets the service
+    internal static func stop() {
+        manager.keys        = []
+        manager.isSyncing   = false
+        removeAllObservers()
+    }
+    
     
     //================================================================================
     // MARK: Helpers
@@ -57,7 +102,7 @@ class iCloudSyncListenr {
         let dict = NSUserDefaults.standardUserDefaults().dictionaryRepresentation()
         
         for (key, value) in dict {
-            if key.hasPrefix(manager.prefix) {
+            if manager.keys.contains(key) {
                 NSUbiquitousKeyValueStore.defaultStore().setObject(value, forKey: key)
             }
             NSUbiquitousKeyValueStore.defaultStore().synchronize()
@@ -70,17 +115,19 @@ class iCloudSyncListenr {
         
         NSNotificationCenter.defaultCenter().removeObserver(self, name: NSUserDefaultsDidChangeNotification, object: nil)
         for (key, value) in dict {
-            if key.hasPrefix(manager.prefix) {
+            if manager.keys.contains(key) {
                 NSUserDefaults.standardUserDefaults().setObject(value, forKey: key)
             }
         }
+        NSNotificationCenter.defaultCenter().postNotificationName(raCloudSyncNotification, object: nil)
+        
         setListenerForNotificationChange()
     }
     
     private static func setListenerFromCloud() {
         NSNotificationCenter.defaultCenter().addObserver(
             self,
-            selector: #selector(iCloudSyncListenr.updateFromiCloud(_:)),
+            selector: #selector(RAiCloudSync.updateFromiCloud(_:)),
             name: NSUbiquitousKeyValueStoreDidChangeExternallyNotification,
             object: nil)
     }
@@ -88,17 +135,25 @@ class iCloudSyncListenr {
     private static func setListenerForNotificationChange() {
         NSNotificationCenter.defaultCenter().addObserver(
             self,
-            selector: #selector(iCloudSyncListenr.updateToiCloud(_:)),
+            selector: #selector(RAiCloudSync.updateToiCloud(_:)),
             name: NSUserDefaultsDidChangeNotification,
             object: nil)
     }
+    
     
     //================================================================================
     // MARK: Deinit
     //================================================================================
     
+    private static func removeAllObservers() {
+        manager.isSyncing = false
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: NSUbiquitousKeyValueStoreDidChangeExternallyNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: NSUserDefaultsDidChangeNotification, object: nil)
+    }
+    
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: NSUbiquitousKeyValueStoreDidChangeExternallyNotification, object: nil)
         NSNotificationCenter.defaultCenter().removeObserver(self, name: NSUserDefaultsDidChangeNotification, object: nil)
     }
+    
 }
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# RichAppz-iCloudSync
+# RAiCloudSync
+
+##What is the purpose?
+
+This is a clean and useful solution to storing `NSUserDefaults` in iCloud, adding keys to the manager will set up a listener for both directions and let you know if there have been any changes.
+
+##Instructions
+
+1. Download and drag the RAiCloudSync to your project
+2. Add to start `RAiCloudSync.start(withKeys: ["key1", "key2", "key3"])`
+3. Use `NSUserDefaults` as normal
+
+####Start
+
+	RAiCloudSync.start(withKeys: ["key1", "key2", "key3"])
+
+####Adding an additional key
+
+	RAiCloudSync.remove(key: "key2")
+		
+####Remove a key
+
+	RAiCloudSync.remove(key: "key2")
+	
+####Stop
+
+	RAiCloudSync.stop()
+
+
+###Legal bit
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+
+PayPal Donations to: [rich@richappz.com](<mailto:rich@richappz.com>)
